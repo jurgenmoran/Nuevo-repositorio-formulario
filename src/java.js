15a5cfc1f380dea368a37fc653d0d96ad1eb8f07
Jurgen Moran <jurgenmoran@gmail.com>
2023-12-03 01:32:17 -0300

    se modifico el java para mensaje si el cliente no llena los campos del formulario

--- src/java.js
+++ src/java.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     var formulario = document.getElementById("formulario-pesquisa");
-  
+
     formulario.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      alert("¡Formulario enviado com sucesso!");
-    });
-  });
+        event.preventDefault();
+
+        alert("¡Formulario enviado com sucesso!");
+    });
+});
+
+const firstName = document.querySelector('#input');
+const button = document.querySelector('#button');
+
+button.addEventListener('click', () => {
+    if (firstName.ariaValueMax.length == 0) {
+        alert('O espaço não pode estar vazío');
+    } else {
+        alert('Hola ' + firstName.value);
+    }
+});
