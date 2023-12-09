ebf64c383d66b6d2a7523c83dc151be8fbbf6687
Jurgen Moran <jurgenmoran@gmail.com>
2023-12-09 04:05:20 -0300

    actualizacion del proyecto

--- src/java.js
+++ src/java.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+/* document.addEventListener("DOMContentLoaded", function () {
     var formulario = document.getElementById("formulario-pesquisa");
 
     formulario.addEventListener("submit", function (event) {
@@ -17,4 +17,21 @@ button.addEventListener('click', () => {
     } else {
         alert('Hola ' + firstName.value);
     }
-});
+}); */
+
+const nome = document.getElementById("nome")
+const email = document.getElementById("email")
+const form = document.getElementById("form")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    if(nome.value.length <3){
+        warnings += `O nome não é valido <br>`
+    }
+
+    if(!regexEmail.test(email.value)){
+        warnings += `O email não é valido <br>`
+    }
+})
