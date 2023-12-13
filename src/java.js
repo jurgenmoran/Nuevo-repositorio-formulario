1ba7ff07e23fb99efe1ccc4740cdc6a4fa3b2b94
Jurgen Moran <jurgenmoran@gmail.com>
2023-12-13 01:03:33 -0300

    proyecto finalizado

--- src/java.js
+++ src/java.js
@@ -1,37 +1,19 @@
-/* document.addEventListener("DOMContentLoaded", function () {
-    var formulario = document.getElementById("formulario-pesquisa");
+const firstName = document.getElementById('nome');
+const button = document.getElementById('botonsubmit');
+
+button.addEventListener("click", () =>{
+    if (firstName.ariaValueMax.length == 0) {
+        
+        alert('O espaço não pode estar vazío');
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var formulario = document.getElementById("form");
 
     formulario.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        alert("¡Formulario enviado com sucesso!");
+        Swal.fire("Seu formulario foi enviado com sucesso!");
     });
 });
-
-const firstName = document.querySelector('#input');
-const button = document.querySelector('#button');
-
-button.addEventListener('click', () => {
-    if (firstName.ariaValueMax.length == 0) {
-        alert('O espaço não pode estar vazío');
-    } else {
-        alert('Hola ' + firstName.value);
-    }
-}); */
-
-const nome = document.getElementById("nome")
-const email = document.getElementById("email")
-const form = document.getElementById("form")
-
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let warnings = ""
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-    if(nome.value.length <3){
-        warnings += `O nome não é valido <br>`
-    }
-
-    if(!regexEmail.test(email.value)){
-        warnings += `O email não é valido <br>`
-    }
-})
